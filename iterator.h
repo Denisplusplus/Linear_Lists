2fbe9deb374d95f32cbf80687a9974de6209b6fb
Denisplusplus <whinslow@mail.ru>
2017-06-02 22:20:02 +0300

    Create iterator.h

---
+++ iterator.h
@@ -0,0 +1,12 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+typedef char Item;
+
+void iterator_first(List* list);
+void iterator_last(List* list);
+void iterator_next(List* list, Node* node);
+
+
+
+#endif
